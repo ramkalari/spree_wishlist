aa95469794a9e584347ee61f564dc672ed453d4b
Sriram Balasubramanian <ramkalari@gmail.com>
2012-09-15 12:53:48 +0530

    add json support and extend api

--- app/controllers/spree/wished_products_controller.rb
+++ app/controllers/spree/wished_products_controller.rb
@@ -1,19 +1,19 @@
-class Spree::WishedProductsController < Spree::BaseController
-  respond_to :html
+class Spree::WishedProductsController < Spree::Api::V1::BaseController
+  respond_to :html, :json
 
   def create
     @wished_product = Spree::WishedProduct.new(params[:wished_product])
-    @wishlist = current_user.wishlist
+    @wishlist = current_api_user.wishlist
 
     if @wishlist.include? params[:wished_product][:variant_id]
       @wished_product = @wishlist.wished_products.detect {|wp| wp.variant_id == params[:wished_product][:variant_id].to_i }
     else
-      @wished_product.wishlist = current_user.wishlist
+      @wished_product.wishlist = current_api_user.wishlist
       @wished_product.save
     end
 
     respond_with(@wished_product) do |format|
-      format.html { redirect_to wishlist_url(@wishlist) }
+     	redirect_to wishlist_url(@wishlist)
     end
   end
 
@@ -22,7 +22,7 @@ class Spree::WishedProductsController < Spree::BaseController
     @wished_product.update_attributes(params[:wished_product])
 
     respond_with(@wished_product) do |format|
-      format.html { redirect_to wishlist_url(@wished_product.wishlist) }
+    	redirect_to wishlist_url(@wished_product.wishlist)
     end
   end
 
@@ -31,7 +31,7 @@ class Spree::WishedProductsController < Spree::BaseController
     @wished_product.destroy
 
     respond_with(@wished_product) do |format|
-      format.html { redirect_to wishlist_url(@wished_product.wishlist) }
+       redirect_to wishlist_url(@wished_product.wishlist)
     end
   end
 end
